ab696cd53f310a729053fc5c56a9df7bfcd97a49
Sungwook Kang <kpuppy110794@gmail.com>
2022-08-21 15:46:10 +0900

    [Fix] Global variable reference error

--- excute_main.py
+++ excute_main.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO
 import sys
 
 # import user_defined_module
-from controlLed import *
+import controlLed
 from imageProcessing import mainVideo
 from config import config
 # mongodb+srv://<username>:<password>@capstoneserver.ujte3.mongodb.net/?retryWrites=true&w=majority
@@ -17,10 +17,16 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # set ledpin
-MAIN_LED = []
-TIME_LED = []
+MAIN_LED = [16,21,20]
+TIME_LED = [19,23,6,5,25,24,23,22,27,17,4]
+
+
 
 if __name__ == '__main__':
+  for PIN in TIME_LED:
+    GPIO.setup(PIN, GPIO.OUT)
+  for PIN in MAIN_LED:
+    GPIO.setup(PIN, GPIO.OUT)
   try:
     client = MongoClient(config.mongoURI)
   except:
@@ -28,9 +34,9 @@ if __name__ == '__main__':
     sys.exit()
   
   
-  t1 = Thread(target=startSignalLed, args = (MAIN_LED, TIME_LED, ))
+  t1 = Thread(target=controlLed.startSignalLed, args = (MAIN_LED, TIME_LED, ))
   t1.start()
-  mainVideo(client, TIME_COUNTER)
+  mainVideo(client, controlLed.TIME_COUNTER)
   t1.join()
 
   GPIO.cleanup()
