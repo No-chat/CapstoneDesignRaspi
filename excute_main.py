adabbc6914562c5345a7028213646b5b28e30d6b
Sungwook Kang <kpuppy110794@gmail.com>
2022-08-25 21:14:38 +0900

    [Fix] Use 3thread

--- excute_main.py
+++ excute_main.py
@@ -1,12 +1,14 @@
 # import installed_module
 from pymongo import MongoClient
 from threading import Thread
+from queue import Queue
 import RPi.GPIO as GPIO
 import sys
 
 # import user_defined_module
 from controlLed import startSignalLed
 from imageProcessing import mainVideo
+from num_extract import extract_thread
 from config import config
 # mongodb+srv://<username>:<password>@capstoneserver.ujte3.mongodb.net/?retryWrites=true&w=majority
 
@@ -31,13 +33,21 @@ def main_func():
   except:
     print('Database connect error')
     sys.exit()
+
+  pipeline = Queue(maxsize=10)
   
   
   t1 = Thread(target=startSignalLed, args = (MAIN_LED, TIME_LED,))
+  t2 = Thread(target=mainVideo, args=(pipeline, ))
+  t3 = Thread(target=extract_thread, args=(pipeline, client, ))
   t1.start()
-  mainVideo(client,)
-  t1.join()
+  t2.start()
+  t3.start()
 
+  t1.join()
+  t2.join()
+  t3.join()
+  
   GPIO.cleanup()
   client.close()
 
